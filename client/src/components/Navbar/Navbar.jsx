214fc2e9eea471d151e3b28802b5a5abbff9a0cd
MinavKaria <minavpkaria@gmail.com>
2024-12-31 16:48:17 +0530

    Add RainbowKit and Wagmi integration; implement ETH transaction feature in Navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { useSendTransaction,useWaitForTransactionReceipt } from 'wagmi'
+import { useAccount } from "wagmi";
+import { ethers } from 'ethers';
+import { ToastContainer, toast } from 'react-toastify';
 
 function Navbar() {
+  const notify = () => toast("Sending you ETH....");
+  const succ = () => toast("1 ETH Sent Successfully");
+  const { address, isConnected, connector,chain } = useAccount();
+
+  const HARDHAT_PRIVATE_KEY="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+
+      const sendEther = async () => {
+        try {
+          const provider = new ethers.JsonRpcProvider(
+            "http://localhost:8545"
+          );
+
+          const signer = new ethers.Wallet(HARDHAT_PRIVATE_KEY, provider);
+
+          const tx = await signer.sendTransaction({
+            to: address,
+            value: ethers.parseEther("1.0"),
+          });
+
+          console.log("Transaction sent:", tx);
+          notify()
+          await tx.wait(); 
+          console.log("Transaction confirmed:", tx.hash);
+          succ()
+          
+        } catch (error) {
+          console.error("Error sending ETH:", error);
+          alert("Failed to send ETH. Check the console for details.");
+        }
+      };
+
+  
+
   return (
-    <>
-        <div>Navbar</div>
+    <> 
+        <div className="flex gap-3 bg-blue-100 justify-end p-3">
+        <ConnectButton/>
+        {(chain && chain.name==="Localhost") && (
+          <>
+            <button className="shadow-md p-2 rounded-lg bg-white flex justify-center items-center" onClick={sendEther}>Get 1<img src="https://cdn3.emoji.gg/emojis/7903-ethereum.png" width="20px" height="20px" alt="Ethereum"/></button>
+        
+          </>
+        )}
+        </div>
+        <ToastContainer />
     </>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,59 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+  sepolia,
+} from "wagmi/chains";
+import { http, createConfig } from "wagmi";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+
+const queryClient = new QueryClient();
+
+export const localhost = {
+  id: 31337, 
+  name: 'Localhost',
+  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+  rpcUrls: {
+    default: { http: ['http://localhost:8545'] },
+  },
+  blockExplorers: {
+    default: { name: 'None', url: '' },
+  },
+  testnet: true,
+};
+
+const config = createConfig({
+  chains: [mainnet, polygon, optimism, arbitrum, base, sepolia,localhost],
+  transports: {
+    [mainnet.id]: http(),
+    [sepolia.id]: http(),
+    [polygon.id]: http(),
+    [optimism.id]: http(),
+    [arbitrum.id]: http(),
+    [localhost.id]: http(),
+  },
+});
+
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+<StrictMode>
+  <WagmiProvider config={config}>
+    <QueryClientProvider client={queryClient}>
+      <RainbowKitProvider>
+        <App />
+      </RainbowKitProvider>
+    </QueryClientProvider>
+  </WagmiProvider>
+</StrictMode>
 )
