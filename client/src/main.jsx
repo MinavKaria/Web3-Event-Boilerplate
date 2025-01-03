b956fc9b8bb0746dc48690c44ee2706a526b4e98
MinavKaria <minavpkaria@gmail.com>
2025-01-03 20:03:31 +0530

    Add small fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import "@rainbow-me/rainbowkit/styles.css";
-import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { getDefaultConfig, RainbowKitProvider, connectorsForWallets } from "@rainbow-me/rainbowkit";
 import { WagmiProvider } from "wagmi";
+import {
+  injectedWallet,
+  walletConnectWallet,
+  coinbaseWallet,
+  metaMaskWallet,
+  braveWallet,
+  rainbowWallet,
+} from "@rainbow-me/rainbowkit/wallets";
 import {
   mainnet,
   polygon,
@@ -20,7 +28,7 @@ import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
 export const localhost = {
-  id: 31337, 
+  id: 31337,
   name: 'Localhost',
   nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
   rpcUrls: {
@@ -32,8 +40,21 @@ export const localhost = {
   testnet: true,
 };
 
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: 'Recommended',
+      wallets: [rainbowWallet, walletConnectWallet, metaMaskWallet, injectedWallet, coinbaseWallet, braveWallet],
+    },
+  ],
+  {
+    appName: 'My RainbowKit App',
+    projectId:'b0f6c378-1c16-41b0-9ee9-2d91a49a01fe=a956e808216d501e98b55e25ee9dfbc086b31a4705479d2296223e1566287bb6 '
+  }
+);
+
 const config = createConfig({
-  chains: [mainnet, polygon, optimism, arbitrum, base, sepolia,localhost],
+  chains: [mainnet, polygon, optimism, arbitrum, base, sepolia, localhost],
   transports: {
     [mainnet.id]: http(),
     [sepolia.id]: http(),
@@ -42,18 +63,19 @@ const config = createConfig({
     [arbitrum.id]: http(),
     [localhost.id]: http(),
   },
+  connectors,
 });
 
 
 
 createRoot(document.getElementById('root')).render(
-<StrictMode>
-  <WagmiProvider config={config}>
-    <QueryClientProvider client={queryClient}>
-      <RainbowKitProvider>
-        <App />
-      </RainbowKitProvider>
-    </QueryClientProvider>
-  </WagmiProvider>
-</StrictMode>
+  <StrictMode>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <App />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  </StrictMode>
 )
